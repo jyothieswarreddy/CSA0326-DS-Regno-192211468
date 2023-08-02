93d65d82d8f956a4db56091899d1e5e017efabcf
jyothieswarreddy <130463245+jyothieswarreddy@users.noreply.github.com>
2023-08-02 20:51:01 +0530

    Add files via upload

---
+++ array ascending order.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int arr[] = {5, 2, 8, 7, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int i, j, temp;
+    
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }  
+    printf("Elements of array sorted in ascending order: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");   
+    return 0;
+}
